47aeffc46f442e19b9454da1867a482d02a62caa
Moses Odeke A <afrawles2@gmail.com>
2026-09-15 10:33:37 +0300

    add nats publish

--- cmd/replay/main.go
+++ cmd/replay/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/fs"
@@ -10,99 +11,144 @@ import (
 	"strings"
 	"time"
 
+	"github.com/nats-io/nats.go"
 	"github.com/parquet-go/parquet-go"
 )
 
 type BranchReading struct {
-	CurrentA float64 //Current_Actual_P1/P2/P3 [A]
-	VoltageV float64 //Voltage_Actual_P1/P2/P3 [V]
+	CurrentA float32 //Current_Actual_P1/P2/P3 [A]
+	VoltageV float32 //Voltage_Actual_P1/P2/P3 [V]
 }
 
 type Reading struct {
-	Cycle int64
-	SemiCycle int64
+	Cycle     string
+	SemiCycle string
 	Timestamp time.Time
 
-	CurrentA float64 //Current_Actual_Battery [A]
-	VoltageV float64 //Voltage_Actual_Battery [V]
+	CurrentA float32 //Current_Actual_Battery [A]
+	VoltageV float32 //Voltage_Actual_Battery [V]
 
-	SocPercent float64 //SoC_Actual_Battery [percent]
+	SocPercent float32 //SoC_Actual_Battery [percent]
 
-	VoltageAvgCell float64
-	VoltageMinCell float64
-	VoltageMaxCell float64
+	VoltageAvgCell float32
+	VoltageMinCell float32
+	VoltageMaxCell float32
 
 	Branches [3]BranchReading
-
 }
 
 type Record struct {
-	Cycle string `parquet:"Cycle"`
-	SemiCycle string `parquet:"SemiCycle"`
+	Cycle     string    `parquet:"Cycle"`
+	SemiCycle string    `parquet:"Semicycle"`
 	Timestamp time.Time `parquet:"Timestamp"`
 
-	CurrentA float32 `parquet:"Current_Actual_Battery [A]"`//Current_Actual_Battery [A]
-	VoltageV float32 `parquet:"Voltage_Actual_Battery [V]"`//Voltage_Actual_Battery [V]
+	CurrentA float32 `parquet:"Current_Actual_Battery [A]"` //Current_Actual_Battery [A]
+	VoltageV float32 `parquet:"Voltage_Actual_Battery [V]"` //Voltage_Actual_Battery [V]
 
 	SocPercent float32 `parquet:"SoC_Actual_Battery [percent]"` //SoC_Actual_Battery [percent]
 
 	VoltageAvgCell float32 `parquet:"Voltage_Avg_Cell [V]"`
 	VoltageMinCell float32 `parquet:"Voltage_Min_Cell [V]"`
 	VoltageMaxCell float32 `parquet:"Voltage_Max_Cell [V]"`
-	
-	CurrentP1A float32 `parquet:"Current_Actual_P1 [A]"`//Current_Actual_P1/P2/P3 [A]
-	CurrentP2A float32 `parquet:"Current_Actual_P2 [A]"`//Current_Actual_P1/P2/P3 [A]
+
+	CurrentP1A float32 `parquet:"Current_Actual_P1 [A]"` //Current_Actual_P1/P2/P3 [A]
+	CurrentP2A float32 `parquet:"Current_Actual_P2 [A]"` //Current_Actual_P1/P2/P3 [A]
 	CurrentP3A float32 `parquet:"Current_Actual_P3 [A]"` //Current_Actual_P1/P2/P3 [A]
-	VoltageP1V float32 `parquet:"Voltage_Actual_P1 [V]"`//Voltage_Actual_P1/P2/P3 [V]
-	VoltageP2V float32 `parquet:"Voltage_Actual_P2 [V]"`//Voltage_Actual_P1/P2/P3 [V]
-	VoltageP3V float32 `parquet:"Voltage_Actual_P3 [V]"`//Voltage_Actual_P1/P2/P3 [V]
+	VoltageP1V float32 `parquet:"Voltage_Actual_P1 [V]"` //Voltage_Actual_P1/P2/P3 [V]
+	VoltageP2V float32 `parquet:"Voltage_Actual_P2 [V]"` //Voltage_Actual_P1/P2/P3 [V]
+	VoltageP3V float32 `parquet:"Voltage_Actual_P3 [V]"` //Voltage_Actual_P1/P2/P3 [V]
 }
 
 func main() {
 	var dataFiles []string
 
-	filepath.WalkDir("data", func(path string, d fs.DirEntry, err error) error {
+	err := filepath.WalkDir("data", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
 		if strings.HasSuffix(path, ".parquet") {
 			dataFiles = append(dataFiles, path)
-		} 
+		}
 
 		return nil
 	})
 
-	f, _ := os.OpenFile(dataFiles[0], os.O_RDONLY, 0400)
-	defer f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	reader := parquet.NewGenericReader[Record](f)
-	defer reader.Close()
+	nc, err := nats.Connect(nats.DefaultURL)
 
-	b := make([]Record, 10)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	for {
-		n, err := reader.Read(b)
-
-		if n == 0 || err == io.EOF {
-			break
-		}
+	for i, v := range dataFiles {
+		f, err := os.OpenFile(v, os.O_RDONLY, 0o400)
 
 		if err != nil {
 			log.Fatal(err)
 		}
+		defer f.Close()
 
-		for i := 0; i < n; i ++ {
-			row := b[i]
+		reader := parquet.NewGenericReader[Record](f)
+		defer reader.Close()
 
-			fmt.Println("*********")
-			fmt.Println(row)
-			fmt.Println("*********")
+		b := make([]Record, 1000)
 
-			break
+		for {
+			n, err := reader.Read(b)
+
+			if n == 0 || err == io.EOF {
+				break
+			}
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for j := 0; j < n; j++ {
+				row := b[i]
+				record := Reading{
+					Cycle:          row.Cycle,
+					SemiCycle:      row.SemiCycle,
+					Timestamp:      row.Timestamp,
+					CurrentA:       row.CurrentA,
+					VoltageV:       row.VoltageV,
+					SocPercent:     row.SocPercent,
+					VoltageAvgCell: row.VoltageAvgCell,
+					VoltageMinCell: row.VoltageMinCell,
+					VoltageMaxCell: row.VoltageMaxCell,
+					Branches: [3]BranchReading{
+						{
+							CurrentA: row.CurrentP1A,
+							VoltageV: row.VoltageP1V,
+						},
+						{
+							CurrentA: row.CurrentP2A,
+							VoltageV: row.VoltageP2V,
+						},
+						{
+							CurrentA: row.CurrentP3A,
+							VoltageV: row.VoltageP3V,
+						},
+					},
+				}
+
+				payload, err := json.Marshal(record)
+				if err != nil {
+					continue
+				}
+
+				err = nc.Publish("bms.reading", payload)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+			}
+
 		}
-
-		break
 	}
-	
+
 }
--- go.mod
+++ go.mod
@@ -3,14 +3,22 @@ module github.com/Afrawles/batteryStats
 go 1.26.2
 
 require (
+	github.com/nats-io/nats.go v1.51.0
+	github.com/parquet-go/parquet-go v0.32.0
+)
+
+require (
 	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/klauspost/compress v1.19.2 // indirect
+	github.com/nats-io/nkeys v0.4.16 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/parquet-go/bitpack v1.0.0 // indirect
 	github.com/parquet-go/jsonlite v1.0.0 // indirect
-	github.com/parquet-go/parquet-go v0.32.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/twpayne/go-geom v1.6.1 // indirect
-	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/crypto v0.55.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 )
